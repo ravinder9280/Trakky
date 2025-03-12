555bd83a2800ea3f5736267dff5a2b09abd090cf
ravinder9280 <ravinder92809@gmail.com>
2025-03-12 20:53:50 +0530

    commit6hjddjjj3

--- app/mealplan/loading.tsx
+++ app/mealplan/loading.tsx
@@ -1,9 +1,32 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 import React from 'react'
 
 const loading = () => {
+    const array=new Array(4).fill(null)
+
   return (
-    <div>loading</div>
-  )
-}
+
+    <div className='flex flex-col w-full p-2 space-y-2'>
+
+
+        {
+             array.map((_, idx) => (
+                        <Card className='w-full' key={idx}>
+                          <CardHeader>
+                            
+                            <Skeleton className='w-[200px] h-4'/>
+                          </CardHeader>
+                          <CardContent  className='space-y-2 w-full'>
+                            <Skeleton className='w-[80vw]  h-4'/>
+                            <Skeleton className='w-[80vw]  h-4'/>
+                            <Skeleton className='w-[80vw]  h-4'/>
+                            
+                            
+                          </CardContent>
+                        </Card>  ))
+        }
+    </div>
+  )}
 
 export default loading
